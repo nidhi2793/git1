5a9d681352cb38f347db0f1e3164c3cde3eedd97
nidhi2793 <MY_NAME@example.com>
2023-05-25 23:26:41 +0530

    tagname

--- dom.js
+++ dom.js
@@ -23,9 +23,15 @@
 
 // title[0].style.color = "green";
 // title[0].style.fontWeight = "bold";
-let items = document.getElementsByClassName("list-group-item");
-console.log(items);
-items[2].style.backgroundColor = "green";
-for (let i = 0; i < items.length; i++) {
-  items[i].style.fontWeight = "bold";
+// let items = document.getElementsByClassName("list-group-item");
+// console.log(items);
+// items[2].style.backgroundColor = "green";
+// for (let i = 0; i < items.length; i++) {
+//   items[i].style.fontWeight = "bold";
+// }
+let li = document.getElementsByTagName("li");
+console.log(li);
+li[2].style.backgroundColor = "green";
+for (let i = 0; i < li.length; i++) {
+  li[i].style.fontWeight = "bold";
 }
