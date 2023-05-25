1db21a853a6c35e62c0d85d7cacfa8e556166e02
nidhi2793 <MY_NAME@example.com>
2023-05-25 22:52:00 +0530

    class

--- dom.js
+++ dom.js
@@ -23,3 +23,9 @@
 
 // title[0].style.color = "green";
 // title[0].style.fontWeight = "bold";
+let items = document.getElementsByClassName("list-group-item");
+console.log(items);
+items[2].style.backgroundColor = "green";
+for (let i = 0; i < items.length; i++) {
+  items[i].style.fontWeight = "bold";
+}
