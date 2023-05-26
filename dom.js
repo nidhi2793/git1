aa8b2e190f1ab0824da09cd94d49f1eb4870e86f
nidhi2793 <MY_NAME@example.com>
2023-05-26 17:15:25 +0530

    parentChildSibling

--- dom.js
+++ dom.js
@@ -51,10 +51,63 @@
 // var thirdItem = document.querySelector(".list-group-item:nth-child(3)");
 // thirdItem.style.visibility = "hidden";
 
-let item = document.querySelectorAll("li");
-item[1].style.color = "green";
+// let item = document.querySelectorAll("li");
+// item[1].style.color = "green";
 
-let odd = document.querySelectorAll("li:nth-child(odd");
-for (var i = 0; i < odd.length; i++) {
-  odd[i].style.backgroundColor = "green";
-}
+// let odd = document.querySelectorAll("li:nth-child(odd");
+// for (var i = 0; i < odd.length; i++) {
+//   odd[i].style.backgroundColor = "green";
+// }
+
+// Traversing dom
+//parent node
+
+let items = document.querySelector("#items");
+// console.log(items.parentNode);
+
+// items.parentNode.style.backgroundColor = "grey";
+// console.log(items.parentNode.parentNode);
+
+console.log(items.parentElement);
+
+items.parentElement.style.backgroundColor = "grey";
+console.log(items.parentElement.parentElement);
+
+console.log(items.childNodes);
+items.childNodes[1].style.color = "red";
+
+console.log(items.lastElementChild);
+
+console.log(items.children[1]);
+items.children[1].style.fontWeight = "bold";
+console.log(items.firstChild);
+console.log(items.firstElementChild);
+items.firstElementChild.style.color = "blue";
+items.lastElementChild.textContent = "Bye";
+
+console.log(items.nextSibling);
+console.log(items.previousSibling);
+console.log(items.previousElementSibling);
+items.previousElementSibling.style.color = "green";
+
+var newDiv = document.createElement("div");
+newDiv.id = "hello";
+newDiv.className = "helloclass";
+newDiv.setAttribute("title", "Hello Div");
+var newDivText = document.createTextNode("Hello");
+newDiv.appendChild(newDivText);
+var container = document.querySelector("header .container");
+var h1 = document.querySelector("header h1");
+console.log(container);
+
+container.insertBefore(newDiv, h1);
+
+let itemdiv = document.createElement("div");
+let itemDivText = document.createTextNode("Hello");
+itemdiv.appendChild(itemDivText);
+itemdiv.style.fontWeight = "bold";
+console.log(itemdiv);
+let item = document.querySelector("li");
+console.log(item);
+let group = document.querySelector("#items");
+group.insertBefore(itemdiv, item);
